a81b1189dae1c3b9fa82d60129c322cc74f2ae97
jusdal <justin_daludado10@icloud.com>
2020-08-06 15:49:57 -0400

    fixed the showHelp function

--- src/com/jd/pwdgen/pwdGenCLI.java
+++ src/com/jd/pwdgen/pwdGenCLI.java
@@ -2,15 +2,33 @@ package com.jd.pwdgen;
 
 public class pwdGenCLI 
 {
-    public static void main(String[] args) 
-    {
-        Pass pass = new Pass(args[0], Integer.valueOf(args[1]), Integer.valueOf(args[2]));
-        pass.generatePasswords();
-        pass.printPasswords();
-    }
-    
-    public static void showHelp(String[] args)
-    {
-    	
-    }
+	public static void main(String[] args) 
+	{
+		boolean cont = true;
+		if (args.length != 3)
+		{
+			showHelp(args);
+			cont = false;
+		}
+		do
+		{
+			Pass pass = new Pass(args[0], Integer.valueOf(args[1]), Integer.valueOf(args[2]));
+			pass.generatePasswords();
+			pass.printPasswords();
+			cont = false;
+		}while(cont = true);
+
+	}
+
+	public static void showHelp(String[] args)
+	{
+		System.out.println("Argument Example: 1234 5 7");
+		System.out.println("The above will generate a password using upper and lowercase characters, numbers. and symbols.");
+		System.out.println("It was also generate SEVEN passwords at a length of FIVE characters.");
+		System.out.println("The first argument will be options for which types of characters you want in your password(s):");
+		System.out.println("\n\t1 will allow for lowercase"
+							+ "\n\t2 will allow for uppercase"
+							+ "\n\t3 will allow for numbers"
+							+ "\n\t4 will allow for symbols");
+	}
 }
